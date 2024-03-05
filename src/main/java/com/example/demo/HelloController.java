6a9f16ee552fb891ff76211d1ef48de9d57851ea
Park Jiho <jiholine10@naver.com>
2024-03-05 17:24:10 +0900

    three

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @GetMapping("/")
     public String home() {
-        return "Hello parkjiho Jenkins!";
+        return "Hello cheer up parkjiho Jenkins!";
     }
 }
